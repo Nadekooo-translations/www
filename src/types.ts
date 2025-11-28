6ff4abc58cf6f719e5040e83c23eba91bc21203a
Vincent <vince@iwit-systems.fr>
2025-11-28 14:00:36 +0100

    wip: continued weblate client

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ import {z} from 'astro:content';
 
 export const TranslationFrontMatter = z.object({
 	name: z.string(),
-	weblateUrl: z.optional(z.string().url()),
+	contributionUrl: z.optional(z.string().url()),
 	externalUrl: z.optional(z.string().url()),
+	weblateComponent: z.optional(z.string()),
 });
--- src/weblateClient.ts
+++ src/weblateClient.ts
@@ -6,3 +6,8 @@ const client = axios.create({
 		Authorization: "Token " + import.meta.env.WEBLATE_TOKEN,
 	},
 });
+
+export const fetchCredits = async (component: string, lang = "fr") => {
+	const res = await client.get(`/components/${component}/credits/?start=2021-01-01&end=2030-01-01&lang=${lang}`);
+	return res.data;
+};
