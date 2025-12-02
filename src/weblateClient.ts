c1efd948842e3d0be86177ca166f050f1f665273
Vincent <vince@iwit-systems.fr>
2025-12-02 13:56:04 +0100

    fix: working weblateClient

--- src/weblateClient.ts
+++ src/weblateClient.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
 
+export interface CreditDatum {
+	email: string;
+	username: string;
+	full_name: string;
+	change_count: number;
+	date_joined: string;
+}
+
+export type CreditLanguage = {[language: string] : CreditDatum[]}
+export type CreditsResponse = CreditLanguage[];
+
 const client = axios.create({
 	baseURL: import.meta.env.WEBLATE_BASE_URL,
 	headers: {
@@ -7,7 +18,9 @@ const client = axios.create({
 	},
 });
 
-export const fetchCredits = async (component: string, lang = "fr") => {
+export const fetchCredits = async (component: string, lang = "fr"): Promise<CreditsResponse> => {
 	const res = await client.get(`/components/${component}/credits/?start=2021-01-01&end=2030-01-01&lang=${lang}`);
 	return res.data;
 };
+
+export const avatar = (username: string) => import.meta.env.WEBLATE_AVATAR_BASE_URL + username + ".png";
