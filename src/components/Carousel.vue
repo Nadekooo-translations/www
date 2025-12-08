bbbfb711b568ee47d515c925da378c0344e0b7c8
Vincent <vince@iwit-systems.fr>
2025-12-08 14:00:01 +0100

    wip: being image carousel

---
+++ src/components/Carousel.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+  const props = defineProps<{images: string[]}>();
+  const images = props.images.map(path => import(path));
+</script>
+<template>
+  <img v-for="image in images" alt="" :src="image" />
+</template>
