f4ddc2709414d0da1368fdbf8d6241aa4372d298
Vincent <vince@iwit-systems.fr>
2025-12-08 13:33:05 +0100

    feat: finished aside

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,7 @@ import {z} from 'astro:content';
 export const TranslationFrontMatter = z.object({
 	name: z.string(),
 	contributionUrl: z.optional(z.string().url()),
+	contributorsUrl: z.optional(z.string().url()),
 	externalUrl: z.optional(z.string().url()),
 	weblateComponent: z.optional(z.string()),
 });
