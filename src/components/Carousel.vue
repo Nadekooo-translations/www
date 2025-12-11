b69187f4940953fc04f32a1a05f3f8103766ef8f
vinceh121 <contact@vinceh121.me>
2025-12-11 20:22:37 +0100

    feat: working carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import {ref} from "vue";
-import type {GetImageResult} from "astro";
-const props = defineProps({images: {type: Array, required: true}}) as {readonly images: Partial<GetImageResult>[]};
+import { ref } from "vue";
+import type { GetImageResult } from "astro";
+const props = defineProps({ images: { type: Array, required: true } }) as { readonly images: Partial<GetImageResult>[] };
 
 const automatic = ref(true);
 const currentImage = ref(0);
@@ -18,7 +18,9 @@ const move = (offset: number) => {
 </script>
 <template>
   <div class="carousel">
-    <img alt="" v-for="(img, idx) in props.images" :src="img.src" v-show="currentImage === idx" />
+    <Transition name="carousel" v-for="(img, idx) in props.images">
+      <img alt="" :src="img.src" :width="img.attributes.width" :height="img.attributes.height" v-show="currentImage === idx" />
+    </Transition>
 
     <div class="overlay">
       <button @click="move(-1)"><i class="ph ph-caret-left"></i></button>
@@ -28,25 +30,57 @@ const move = (offset: number) => {
 </template>
 <style lang="scss">
 @use "../styles/colors";
+@use "sass:color";
 
-$height: 30ch;
+$height: 45ch;
 
 div.carousel {
   display: flex;
   height: $height;
-  width: 100%;
+  width: 75ch;
+  user-select: none;
+  justify-content: center;
+
+  background: colors.$stone-900;
 
   img {
+    position: absolute;
     height: $height;
     width: auto;
+
+    &.carousel-enter-active,
+    &.carousel-leave-active {
+      transition: opacity 0.5s ease;
+    }
+
+    &.carousel-enter-from,
+    &.carousel-leave-to {
+      opacity: 0;
+    }
   }
 
   div.overlay {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    z-index: 10;
+    pointer-events: none;
+
     button {
+      height: 100%;
       color: colors.$white;
       background: none;
       border: none;
       font-size: 2em;
+
+      text-shadow: 0 0 4px black;
+      cursor: pointer;
+      pointer-events: all;
+
+      &:hover {
+        background: color.change(colors.$stone-950, $alpha: 50%);
+      }
     }
   }
 }
