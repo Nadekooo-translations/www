81af66f51ecdcacbd91f934c0092fc73584085a7
vinceh121 <contact@vinceh121.me>
2025-12-14 18:25:15 +0100

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import vue from "@astrojs/vue";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), vue()],
+  site: "https://nad-staging.vinceh121.me",
+  integrations: [mdx(), vue(), sitemap()],
   image: {
     domains: ["weblate.vinceh121.me"],
   },
